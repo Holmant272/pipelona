c6a3846f6f16b253024b4a49ec1f44c0997e3549
julianblanco7 <jorjuela@uniempresarial.edu.co>
2024-11-20 12:07:35 -0500

    dfsgffg

--- src/main/java/co/edu/EntidadFinanciera/infraestructura/TipoPersonaRest.java
+++ src/main/java/co/edu/EntidadFinanciera/infraestructura/TipoPersonaRest.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
+
+/*asdasdas*/
+
 @CrossOrigin(origins = "*", maxAge = 3600) 
 @RestController 
 @RequestMapping({"/entidadfinanciera"})
