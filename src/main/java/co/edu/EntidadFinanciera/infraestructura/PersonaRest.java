e206108a59b87e9f7483105d98fd62c0cf1b5653
julianblanco7 <jorjuela@uniempresarial.edu.co>
2024-11-25 15:26:13 -0500

    hola

--- src/main/java/co/edu/EntidadFinanciera/infraestructura/PersonaRest.java
+++ src/main/java/co/edu/EntidadFinanciera/infraestructura/PersonaRest.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping({"/entidadfinanciera"}) 
 
 public class PersonaRest { 
-    
+    //felipe
      @Autowired 
     PersonaService service; 
  
