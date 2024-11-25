2647f69bae70f5f5573807b8db7a2752e190daa6
julianblanco7 <jorjuela@uniempresarial.edu.co>
2024-11-25 14:08:33 -0500

    hola

--- src/main/java/co/edu/EntidadFinanciera/infraestructura/PersonaRest.java
+++ src/main/java/co/edu/EntidadFinanciera/infraestructura/PersonaRest.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController; 
-//felipe
+
 @CrossOrigin(origins = "*", maxAge = 3600) 
 @RestController 
 @RequestMapping({"/entidadfinanciera"}) 
